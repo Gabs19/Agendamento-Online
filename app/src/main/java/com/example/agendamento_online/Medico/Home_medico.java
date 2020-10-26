518bf4e5568af060f96c513916cc4332d7f9c2f1
Gaston Gouveia <gastongouveia@gmail.com>
2020-10-26 05:22:22 -0300

    Inclusão da Home Médicos

--- app/src/main/java/com/example/agendamento_online/Medico/Home_medico.java
+++ app/src/main/java/com/example/agendamento_online/Medico/Home_medico.java
@@ -2,17 +2,30 @@ package com.example.agendamento_online.Medico;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.agendamento_online.ConsultaAdapter;
 import com.example.agendamento_online.MainActivity;
+import com.example.agendamento_online.Model.Consulta;
 import com.example.agendamento_online.R;
 import com.example.agendamento_online.authentication.Conexao;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home_medico extends AppCompatActivity {
 
@@ -21,14 +34,19 @@ public class Home_medico extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference databaseReference;
 
-    TextView medico;
+    private List<Consulta> consultas = new ArrayList<Consulta>();
+    private ConsultaAdapter consultaArrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_medico);
 
-        medico = findViewById(R.id.medico);
+        consultaArrayAdapter = new ConsultaAdapter(consultas);
+        RecyclerView rv = findViewById(R.id.recycler_view_main);
+        rv.setAdapter(consultaArrayAdapter);
+
     }
 
     protected void onStart() {
@@ -43,9 +61,22 @@ public class Home_medico extends AppCompatActivity {
 
     private void verificarUser() {
 
-        if (user == null) {
-            Intent principal = new Intent(Home_medico.this, MainActivity.class);
-            startActivity(principal);
-        }
+        Query consultaQuery = databaseReference.child("Consulta").orderByChild("id_medico").equalTo(user.getUid());
+
+        consultaQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot obj : snapshot.getChildren()) {
+
+                    Consulta consulta = obj.getValue(Consulta.class);
+                    consultas.add(consulta);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
     }
 }
